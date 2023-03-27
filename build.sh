30ec83f6ce22ad701753f03e5091007ef91c3413
Phillip Schichtel <phillip@schich.tel>
2023-03-27 21:35:45 +0200

    forgot the login

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin ghcr.io
 config=web_only
 version=stable
 build_root="build-root"
